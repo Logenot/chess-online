c3ede67f0ae8851bc40646952d80be0445942a87
Георгий <gpl1@tpu.ru>
2025-06-25 10:14:20 +0700

    37.3

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const waitingPlayers = new Map();
+
 const app = express();
 
 // Укажите свой фронтенд-домен
@@ -22,6 +22,8 @@ const io = new Server(server, {
 // ---- Состояние комнат и пользователей ----
 const rooms = {};
 const leaderboard = {};
+const waitingPlayers = new Map(); // ID сокета -> никнейм
+
 function recordResult(winner, loser) {
   [winner, loser].forEach(nick => {
     if (!leaderboard[nick]) {
@@ -49,45 +51,59 @@ app.get('/leaderboard', (req, res) => {
 // ---- WebSocket логика ----
 io.on('connection', (socket) => {
   console.log('👤 New user connected:', socket.id);
-  socket.on('findOpponent', ({ nickname }) => {
-    socket.data.nickname = nickname;
-  
-    if (waitingPlayers.size > 0) {
-      const [opponentId, opponentNick] = waitingPlayers.entries().next().value;
-      waitingPlayers.delete(opponentId);
-      
-      const roomId = `auto_${Date.now()}`;
-      rooms[roomId] = [
-        { id: opponentId, nickname: opponentNick },
-        { id: socket.id, nickname }
-      ];
-    
-      const opponentSocket = io.sockets.sockets.get(opponentId);
-      opponentSocket.join(roomId);
-      socket.join(roomId);
-    
-      io.to(opponentId).emit('joinAutoRoom', roomId);
-      socket.emit('joinAutoRoom', roomId);
-    } else {
-      waitingPlayers.set(socket.id, nickname);
-    }
-  });
-
-  socket.on('cancelSearch', () => {
-    waitingPlayers.delete(socket.id);
-  });
 
   socket.on('joinRoom', ({ roomId, nickname }) => {
     socket.data.nickname = nickname;
 
     if (!rooms[roomId]) rooms[roomId] = [];
-    rooms[roomId].push({ id: socket.id, nickname }); // 👈 Сохраняем как объект
+    rooms[roomId].push({ id: socket.id, nickname });
     socket.join(roomId);
 
     console.log(`📥 ${socket.id} (${nickname}) joined room ${roomId}`);
     io.to(roomId).emit('roomUpdate', {
-      players: rooms[roomId], // 👈 Теперь отправляется массив объектов
+      players: rooms[roomId],
     });
+  });
+
+  socket.on('findOpponent', ({ nickname }) => {
+    socket.data.nickname = nickname;
+    
+    // Проверяем есть ли ожидающие игроки
+    if (waitingPlayers.size > 0) {
+      // Берём первого ожидающего
+      const [opponentId, opponentNick] = waitingPlayers.entries().next().value;
+      waitingPlayers.delete(opponentId);
+      
+      // Создаем комнату
+      const roomId = `auto_${Date.now()}`;
+      rooms[roomId] = [
+        { id: opponentId, nickname: opponentNick },
+        { id: socket.id, nickname }
+      ];
+      
+      // Присоединяем обоих игроков
+      const opponentSocket = io.sockets.sockets.get(opponentId);
+      opponentSocket.join(roomId);
+      socket.join(roomId);
+      
+      // Уведомляем игроков
+      io.to(opponentId).emit('joinAutoRoom', roomId);
+      socket.emit('joinAutoRoom', roomId);
+      
+      console.log(`🤝 Created auto room ${roomId} for ${nickname} and ${opponentNick}`);
+    } else {
+      // Добавляем в очередь ожидания
+      waitingPlayers.set(socket.id, nickname);
+      console.log(`⏳ Player ${nickname} added to waiting queue`);
+    }
+  });
+
+  socket.on('cancelSearch', () => {
+    if (waitingPlayers.has(socket.id)) {
+      const nickname = waitingPlayers.get(socket.id);
+      waitingPlayers.delete(socket.id);
+      console.log(`❌ ${nickname} canceled search`);
+    }
   });
 
   socket.on('move', ({ roomId, move }) => {
@@ -98,18 +114,46 @@ io.on('connection', (socket) => {
   socket.on('gameOver', ({ roomId, winner, loser }) => {
     console.log(`🏁 Game over in room ${roomId}. Winner: ${winner}, Loser: ${loser}`);
     recordResult(winner, loser);
+    
+    // Удаляем комнату после завершения игры
+    if (rooms[roomId]) {
+      delete rooms[roomId];
+      console.log(`🗑 Room ${roomId} deleted after game over`);
+    }
   });
 
   socket.on('disconnect', () => {
     console.log('❌ User disconnected:', socket.id);
-    // В обработчике disconnect:
-    socket.on('disconnect', () => {
+    
+    // Удаляем из очереди ожидания при отключении
+    if (waitingPlayers.has(socket.id)) {
+      const nickname = waitingPlayers.get(socket.id);
       waitingPlayers.delete(socket.id);
+      console.log(`🚮 ${nickname} removed from waiting queue (disconnected)`);
+    }
+    
+    // Удаляем из комнат и уведомляем других игроков
     for (const roomId in rooms) {
-      rooms[roomId] = rooms[roomId].filter(player => player.id !== socket.id);
-      io.to(roomId).emit('roomUpdate', {
-        players: rooms[roomId],
-      });
+      const roomPlayers = rooms[roomId];
+      const playerIndex = roomPlayers.findIndex(player => player.id === socket.id);
+      
+      if (playerIndex !== -1) {
+        const nickname = roomPlayers[playerIndex].nickname;
+        rooms[roomId] = roomPlayers.filter(player => player.id !== socket.id);
+        
+        // Уведомляем оставшихся игроков
+        io.to(roomId).emit('roomUpdate', {
+          players: rooms[roomId],
+        });
+        
+        console.log(`🚪 ${nickname} left room ${roomId} (disconnected)`);
+        
+        // Удаляем комнату если остался 1 игрок
+        if (rooms[roomId].length < 2) {
+          delete rooms[roomId];
+          console.log(`🗑 Room ${roomId} deleted (not enough players)`);
+        }
+      }
     }
   });
 });
